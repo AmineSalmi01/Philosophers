6af1d0382a4302233734c6d3031f6427e21e14ad
AmineSalmi01 <amine.salmi.solicode@gmail.com>
2024-07-21 21:08:23 +0100

    akhur makayn

--- philo/philo.c
+++ philo/philo.c
@@ -1,19 +1,22 @@
 #include "Philosophers.h"
 
+void free_philo(t_data *data)
+{
+    free(data->philo);
+}
 void all()
 {
     system ("Leaks philo");
 }
 int main(int ac, char **av)
 {
-    // atexit(all);
+    atexit(all);
     t_data data;
 
-    if (ac != 5 && ac != 6)
+    if (ac != 5 && ac != 6) 
         printf("Error: Invalid arguments\n");
     if (!check_args(av))
         printf("is not a number");
     init_struct(&data, av);
-    for (int i = 0; i < data.n_philo; i++)
-        printf("philo [%d] ", data.philo[i].id);
+    free_philo(&data);
 }
